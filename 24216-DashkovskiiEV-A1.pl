c6a3de769c2e8afa4b6fd9e25587bea2a9b99dc5
Egor Dashkovskii <e.dashkovskii1@g.nsu.ru>
2025-09-19 21:21:50 +0700

    fix

--- 24216-DashkovskiiEV-A1.pl
+++ 24216-DashkovskiiEV-A1.pl
@@ -1,14 +1,23 @@
 sol(Solution) :-
+    member(Age1, [7,4,10,13]), member(Age2, [7,4,10,13]), member(Age3, [7,4,10,13]), member(Age4, [7,4,10,13]),
+    Age1 \= Age2, Age1 \= Age3, Age1 \= Age4, Age2 \= Age3, Age2 \= Age4, Age3 \= Age4,
+
+    member(Country1, [amb, ken, bas, tar]), member(Country2, [amb, ken, bas, tar]),
+    member(Country3, [amb, ken, bas, tar]), member(Country4, [amb, ken, bas, tar]),
+    Country1 \= Country2, Country1 \= Country3, Country1 \= Country4,
+    Country2 \= Country3, Country2 \= Country4, Country3 \= Country4,
+
+    member(Keeper1, [eva, gracie, dolly, francine]), member(Keeper2, [eva, gracie, dolly, francine]),
+    member(Keeper3, [eva, gracie, dolly, francine]), member(Keeper4, [eva, gracie, dolly, francine]),
+    Keeper1 \= Keeper2, Keeper1 \= Keeper3, Keeper1 \= Keeper4,
+    Keeper2 \= Keeper3, Keeper2 \= Keeper4, Keeper3 \= Keeper4,
+
     Solution = [
         Age1-shamir-Keeper1-Country1,
         Age2-ofallo-Keeper2-Country2,
         Age3-quirrel-Keeper3-Country3,
         Age4-merah-Keeper4-Country4
     ],
-
-    permutation([7,4,10,13], [Age1, Age2, Age3, Age4]),
-    permutation([amb, ken, bas, tar], [Country1, Country2, Country3, Country4]),
-    permutation([eva, gracie, dolly, francine], [Keeper1, Keeper2, Keeper3, Keeper4]),
 
     % 1. Шамиру 7 лет
     member(7-shamir-_-_, Solution),
@@ -23,7 +32,7 @@ sol(Solution) :-
     ( 
         member(13-ofallo-_-_, Solution), member(_-NameTar-gracie-tar, Solution),
         dif(NameTar, ofallo);
-        member(_ -ofallo-gracie-_, Solution), member(13-NameTar-_-tar, Solution),
+        member(_-ofallo-gracie-_, Solution), member(13-NameTar-_-tar, Solution),
         dif(NameTar, ofallo)
     ),
 
@@ -31,7 +40,7 @@ sol(Solution) :-
     (member(10-_-_-amb, Solution); member(_-_-francine-amb, Solution)),
 
     % 6. Офалло не десятилетний.
-    (member(AgeOf-ofallo-_-_, Solution), not(AgeOf = 10)),
+    (member(AgeOf-ofallo-_-_, Solution), AgeOf \= 10),
 
     % 7. Обезьяна из Кендиси старше, чем та, за которой ухаживает Долли
     member(AgeK-_-_-ken, Solution),
